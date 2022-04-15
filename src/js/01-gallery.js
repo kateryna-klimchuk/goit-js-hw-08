e49bdd77c69334b3e58626c50ce100843367ffb6
kateryna klimchuk <ketyclima@gmail.com>
2022-04-15 22:37:58 +0300

    complited first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+
+const imagesMarkUp = galleryItems.map(({ preview, original, description }) =>
+    `<a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a>`).join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', imagesMarkUp);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: "250",
+    enableKeyboard: "true",
+});
