b0b275d68f8389136156089a78cc039108e57665
kateryna klimchuk <ketyclima@gmail.com>
2022-04-28 16:08:59 +0300

    second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,41 @@
 
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const throttle = require('lodash.throttle');
+
+const SET_TIME = "videoplayer-current-time";
+
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+
+const onPlay = function (evt) {
+    localStorage.setItem(SET_TIME, JSON.parse(evt.seconds));
+};
+
+    const currentTime = localStorage.getItem(SET_TIME);
+
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(currentTime).catch(function (error) {
+    switch (error.name) {
+    case 'RangeError':
+        console.log('Wrong time!');
+        break;
+
+    default:
+        console.log('Error!');
+        break;
+    }
+});
+
+
+
